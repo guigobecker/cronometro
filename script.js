1cb6f536b95f79257e95f2d55b6f30ffc6102cbc
Guigo <rsbecker@inf.ufsm.br>
2024-03-10 16:31:08 -0300

    Corrigir formatação para minutos e segundos

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const relogio = document.getElementById('relogio');
 let intervalo = null;
 let segundosPassados = 0;
+let minutos = 0;
+let segundos = 0;
 
 function atualizaTela() {
-  relogio.innerHTML = segundosPassados;
+  minutos = Math.floor(segundosPassados / 60);
+  segundos = segundosPassados % 60;
+  relogio.innerHTML = `${minutos}:${segundos}`;
 }
 
 function atualizaSegundosPassados(){
